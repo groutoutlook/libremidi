4a4094e8944d253f62f93b10477b56c5dba0a605
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2025-04-22 15:35:09 +0700

    refactor: elaborate the midiout test case.

--- examples/midiout.cpp
+++ examples/midiout.cpp
@@ -37,10 +37,12 @@ auto playMidiSequence(
 
   for (const auto& [time, note, velocity] : sequence)
   {
+    // Yield MIDI event for processing, allowing external control over execution timing
+    co_yield std::make_tuple(time, note, velocity);
+    // Check if it's time to play the note
     while (std::chrono::steady_clock::now() < start_time + std::chrono::milliseconds(time))
-    {
-      co_yield std::make_tuple(time, note, velocity); // Yield event for processing
-    }
+      std::this_thread::yield(); // Allow other tasks to proceed
+
     midiout.send_message(144, note, velocity); // Note On
     std::this_thread::sleep_for(std::chrono::milliseconds(300));
     midiout.send_message(128, note, velocity / 2); // Note Off
@@ -99,46 +101,48 @@ int main(int argc, const char** argv)
     std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 
-  std::vector<std::tuple<int, int, int>> happy_birthday = {
-      {0, 60, 100},    // C
-      {400, 60, 100},  // C
-      {800, 62, 100},  // D
-      {1200, 60, 100}, // C
-      {1600, 65, 100}, // F
-      {2000, 64, 100}, // E
+std::vector<std::tuple<int, int, int>> happy_birthday_harmony = {
+    // Opening phrase
+    {0, 60, 127}, {0, 64, 90},  // C and E (harmony)
+    {400, 60, 127}, {400, 67, 80}, // C and G
+    {800, 62, 127}, {800, 65, 100},  // D and F
+    {1200, 60, 127}, {1200, 64, 90},  // C and E
+    {1600, 65, 127}, {1600, 69, 80}, // F and A
+    {2000, 64, 127}, {2000, 72, 90}, // E and C (higher)
 
-      {2800, 60, 100}, // C
-      {3200, 60, 100}, // C
-      {3600, 62, 100}, // D
-      {4000, 60, 100}, // C
-      {4400, 67, 100}, // G
-      {4800, 65, 100}, // F
+    // Second phrase
+    {2800, 60, 127}, {2800, 64, 85}, // C and E
+    {3200, 60, 127}, {3200, 67, 80}, // C and G
+    {3600, 62, 127}, {3600, 65, 100}, // D and F
+    {4000, 60, 127}, {4000, 64, 90}, // C and E
+    {4400, 67, 127}, {4400, 72, 85}, // G and C (higher)
+    {4800, 65, 127}, {4800, 69, 80}, // F and A
 
-      {5600, 60, 100}, // C
-      {6000, 60, 100}, // C
-      {6400, 72, 100}, // High C
-      {6800, 69, 100}, // A
-      {7200, 65, 100}, // F
-      {7600, 64, 100}, // E
-      {8000, 62, 100}, // D
+    // Third phrase - climactic high note
+    {5600, 60, 127}, {5600, 64, 85}, // C and E
+    {6000, 60, 127}, {6000, 67, 80}, // C and G
+    {6400, 72, 127}, {6400, 76, 80}, // High C and F (powerful ending)
+    {6800, 69, 127}, {6800, 65, 90}, // A and F
+    {7200, 65, 127}, {7200, 72, 85}, // F and C
+    {7600, 64, 127}, {7600, 67, 80}, // E and G
+    {8000, 62, 127}, {8000, 69, 85}, // D and A
 
-      {8800, 70, 100},  // Bb
-      {9200, 70, 100},  // Bb
-      {9600, 69, 100},  // A
-      {10000, 65, 100}, // F
-      {10400, 67, 100}, // G
-      {10800, 65, 100}  // F
-  };
+    // Final closing phrase
+    {8800, 70, 127}, {8800, 65, 80}, // Bb and F
+    {9200, 70, 127}, {9200, 64, 85}, // Bb and E
+    {9600, 69, 127}, {9600, 72, 80}, // A and C
+    {10000, 65, 127}, {10000, 69, 90}, // F and A
+    {10400, 67, 127}, {10400, 72, 85}, // G and C
+    {10800, 65, 127}, {10800, 69, 80} // F and A
+};
 
-  auto midi_gen = playMidiSequence(midiout, happy_birthday);
+  auto midi_gen = playMidiSequence(midiout, happy_birthday_harmony);
 
   // Process MIDI events asynchronously
   for (auto [time, note, velocity] : midi_gen)
   {
     std::cout << "Playing: " << note << " at " << time << "ms\n";
   }
-  std::this_thread::sleep_for(500ms);
-
   // Control Change: 176, 7, 40
   midiout.send_message(176, 7, 40);
 
